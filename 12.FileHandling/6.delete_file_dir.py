266d7f817ba48b9a8977da2ab1352bd7732b654e
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-07-06 21:09:38 +0530

    complete file handling

---
+++ 12.FileHandling/6.delete_file_dir.py
@@ -0,0 +1,17 @@
+'''
+os.remove(path) # deletes a file
+os.rmdir(path)  # deletes an empty directory
+shutil.rmtree(path) #deletes a dir containing files !This is to be used with precaution
+'''
+
+import os 
+import shutil
+
+# path='copy.txt'
+# os.remove(path) #works
+
+# path='delete_folder'
+# os.rmdir(path)# works
+
+# path='folder'
+# shutil.rmtree(path)# works
