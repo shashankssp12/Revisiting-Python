e50d10b06f96bd3abdb62a39cd19a89cdffc3d63
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-07-06 16:10:34 +0530

    .format().py

---
+++ 9.str.format().py
@@ -0,0 +1,51 @@
+'''
+.format()--> gives more control over the presentation of the string.
+
+'''
+# text="The big brown fox jumps over the lazy dog"
+# #What if we want to have animals of our choice.
+
+animal1="dog"
+animal2="fox"
+## Way 1--> By using positional arguments
+# print("The {} jumps over the {}".format(animal1,animal2))
+
+# # Better way
+# text="The {} jumps over the {}"
+# print(text.format(animal1,animal2))
+
+# # Can give numbering as well in the string 
+# text="The {0} jumps over the {0}"
+# print(text.format(animal1,animal2))
+
+# print(text.format(animal1,animal2))
+
+## Way2 --> By using keyword arguments in a btter way
+
+# text="The {a} jumps over the {b}"
+# print(text.format(a="Elephant",b="Rhino"))
+
+# ---------------------------# #Flexibile Formatting
+'''
+{:}
+{:5}
+{:>5}
+'''
+a="Elephant"
+# text="The {:55} jumps!"
+print("The {:20}jumps!".format(a))
+print("The {:<20}jumps!".format(a))
+print("The {:>20}jumps!".format(a))
+print("The {:^20}jumps!".format(a))
+
+# -----------With Numbers-------------
+
+number=3.14159
+print("The number pi is {:.2f}".format(number))
+number2=14234
+print("Number is {:,}".format(number2))
+print("Number is {:b}".format(number2)) #gives binary of the number
+
+'''
+ method in Python returns a new string with the formatted values. It does not modify the original string but instead creates and returns a new one.
+ '''
