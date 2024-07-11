ea7de2767be93e4f2ba09868bf892c98b784b7f9
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-07-11 13:13:30 +0530

    minor add-ons

--- P.O.O.P/2.Inheritance(All Types)/animals.py
+++ P.O.O.P/2.Inheritance(All Types)/animals.py
@@ -1,4 +1,25 @@
+'''
+INHERITANCE - The child class has access to all the properties of the parent class and obviously have access to methods of itself.
+
+
+'''
+
 class Animals:
     alive = True
 
-#TODO
+class Humans(Animals):
+    def creates(self):
+        print("This human can create")
+
+class Dog(Animals):
+    def bark(self):
+        print("This dog barks")
+        
+        
+dog1 = Dog()
+print("Is this dog alive?",dog1.alive)
+dog1.bark();
+
+h1 = Humans()
+print("Is this human alive?", h1.alive)
+h1.creates();
