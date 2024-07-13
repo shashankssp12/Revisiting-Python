db714cfc73bcbf907ebbeb6a447d782992bde71e
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-07-13 22:51:38 +0530

    reduce function to do

---
+++ Advance Concepts/reduce_function.py
@@ -0,0 +1,11 @@
+'''
+reduce(): This function works on the first two elements of the iterable and works upon them and turns it into a single value . The Process Repeats until only one element in left.
+
+reduce(function,iterable)
+must import functools
+
+'''
+import functools
+
+# The  usual way of adding all elements in a list: 
+list1=['H','E','L','L','O']
