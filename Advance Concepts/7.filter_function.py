57b39f845fe473323173dd5f1029a84050242bfc
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-07-13 09:23:58 +0530

    filter function

--- Advance Concepts/7.filter_function.py
+++ Advance Concepts/7.filter_function.py
@@ -1,1 +1,21 @@
-#todo
+'''
+filter(function,iterable)--> Creates a collection of elements from an iterable for which the funciton returns True.
+
+returns a filter object--->can be casted in list or so..
+'''
+
+list_items = [
+        ("name1",15),
+        ("name2",19),
+        ("name3",21),
+        ("name4",17),
+        ("name5",18)
+       ]
+age_check = lambda data:data[1] >= 18
+
+print(list(filter(age_check,list_items)))
+
+
+
+
+
