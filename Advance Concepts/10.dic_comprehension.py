438722e14207061f388e39e645e9223990d70e4a
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-07-14 11:13:13 +0530

    dict comprehension

--- Advance Concepts/10.dic_comprehension.py
+++ Advance Concepts/10.dic_comprehension.py
@@ -9,4 +9,59 @@ WAYS:
     
     **if iterable is dictionary use .items
 '''
+# EXAMPLE 1
+cities_in_F = {
+                "New York": 80,
+                "Los Angeles": 75,
+                "Chicago": 72,
+                "Miami": 85,
+                "Dallas": 90
+              }
+print(cities_in_F)
+cities_in_C = {key:round((value-32)*(5/9)) for (key,value) in cities_in_F.items()}
+print(cities_in_C)
 
+# -------------------------- EXAMPLE 2
+weather_cities = {
+                "New York": "cold",
+                "Los Angeles": "hot",
+                "Chicago": "cold",
+                "Miami": "hot",
+                "Dallas": "cold"
+              }
+weather = {key: value for (key,value) in weather_cities.items() if value == "hot" }
+print(weather)
+
+# -------------------------- EXAMPLE 3
+
+temperature = {
+                "New York": 80,
+                "Los Angeles": 75,
+                "Chicago": 72,
+                "Miami": 85,
+                "Dallas": 90
+              }
+print(temperature)
+temp_check = {key: "hot" if value >=80 else "cold" for (key,value) in temperature.items()}
+print(temp_check)
+
+# -------------------------- EXAMPLE 4
+
+# calling a function: in a dictionary
+
+def check_temp(value):
+    if value >= 80:
+        return "hot"
+    else:
+        return "coldddd"
+    
+temperature_cities = {
+                "New York": 80,
+                "Los Angeles": 75,
+                "Chicago": 72,
+                "Miami": 85,
+                "Dallas": 90
+              }
+print(temperature)
+temp_show = {key: check_temp(value) for (key,value) in temperature.items()}
+print(temp_show)
