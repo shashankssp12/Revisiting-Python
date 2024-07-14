8f767ad38923ec555c521d8007f12b15c8101211
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-07-14 13:20:27 +0530

    continue

--- 6.Advance Concepts (Part-2)/tempCodeRunnerFile.py
+++ 6.Advance Concepts (Part-2)/tempCodeRunnerFile.py
@@ -1,2 +1,6 @@
-
-print(__name__)
+name="Bro Code"
+[inclusive:exclusive:step]
+print(name[:3]+ name[4:])
+print(name[::2])
+# Reversed name:
+print(name[::-1])
--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-name="Bro Code"
-[inclusive:exclusive:step]
-print(name[:3]+ name[4:])
-print(name[::2])
-# Reversed name:
-print(name[::-1])
