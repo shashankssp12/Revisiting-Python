2c9aa79df6a99dcb9caa054c770c02077cec6f93
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-07-30 10:02:17 +0530

    best practices | conventions

---
+++ 1.Basics/0.1.naming_convention.py
@@ -0,0 +1,39 @@
+'''
+Python follows PEP8(Python Enhancement Propsal) style for naming convention:
+
+1.Variable Names: snake_case
+    Example: user_name, total_amount
+2.Constant Names: CAPITAL_SNAKE_CASE
+    Example: MAX_CONNECTIONS, DEFAULT_TIMEOUT
+3.Function/Method Names: snake_case()
+    Example: calculate_total, get_user_input
+4.Class Names: CamelCase
+    ex: MyClass, RecipeManager
+5.Module/Package Names: snake_case.py
+    Example: my_module, recipe_manager
+'''
+
+#--------------------What is PEP8?-----------------------------
+'''
+Set of guidelines and best practices for writing python code, to maintain consistency and readability.
+Key Elements: 
+1 Indentation: 
+    Use 4 spaces per indentation level.
+2 Line Length:
+    Limit lines to 79 characters.
+3 Blank Lines:
+    Use blank lines to separate top-level function and class definitions.
+4 Imports:  
+    Import standard libraries first, followed by third-party libraries, and then local imports.Each group should be separated by a blank line.
+5 Whitespace:
+    Avoid extraneous whitespace in expressions and statements.
+    Use a single space around operators and after commas.
+6 Comments:
+    Write comments that are clear and concise.
+    Use docstrings for documenting modules, classes, and functions.
+7 Naming Conventions:
+    Follow specific naming conventions for different entities (as previously mentioned).
+8 Code Layout:
+    Use consistent and readable code layout.
+    Keep related code together.
+'''
